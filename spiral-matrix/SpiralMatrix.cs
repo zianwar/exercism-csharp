8e1cebed03dafd7efed53ed7c83d465a12151cc8
Anwar Ziani <anwaar.ziani@gmail.com>
2020-04-01 20:22:39 -0700

    Add spiral-matrix exercice

---
+++ spiral-matrix/SpiralMatrix.cs
@@ -0,0 +1,33 @@
+﻿
+public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        var m = new int[size, size];
+
+        int left = 0, top = 0;
+        int right = size - 1, bottom = size - 1;
+        int n = 1;
+
+        while (left <= right && top <= bottom)
+        {
+            for (int i = left; i <= right; i++)
+                m[top, i] = n++;
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                m[i, right] = n++;
+            right--;
+
+            for (int i = right; i >= left; i--)
+                m[bottom, i] = n++;
+            bottom--;
+
+            for (int i = bottom; i >= top; i--)
+                m[i, left] = n++;
+            left++;
+        }
+
+        return m;
+    }
+}
