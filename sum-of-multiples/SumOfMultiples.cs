b91e37cde43f9e5576fc16aff9214fdcd198e99f
Anwar Ziani <anwaar.ziani@gmail.com>
2020-04-02 12:03:02 -0700

    Add sum-of-multiples exercice

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+        => Enumerable.Range(0, max)
+            .Where(i => multiples.Where(i => i != 0).Any(m => i % m == 0))
+            .Sum();
+}
